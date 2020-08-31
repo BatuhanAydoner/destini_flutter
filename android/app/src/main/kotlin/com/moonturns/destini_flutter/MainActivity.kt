a7b5d9c19b0423b0fa73854a630a571e8c1e9fd8
DESKTOP-IU11CJP\batuh <batuhanaydonerg1998@gmail.com>
2020-08-31 13:26:36 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/moonturns/destini_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.moonturns.destini_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DestiniApp());
+}
+
+class DestiniApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class DestiniPage extends StatefulWidget {
+  @override
+  _DestiniPageState createState() => _DestiniPageState();
+}
+
+class _DestiniPageState extends State<DestiniPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
