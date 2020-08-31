573686f90f6020eba85aabf3dc81b90c325d11bf
DESKTOP-IU11CJP\batuh <batuhanaydonerg1998@gmail.com>
2020-08-31 13:49:13 +0300

    Setup UI

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,11 @@ void main() {
 class DestiniApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: DestiniPage(),
+      ),
+    );
   }
 }
 
@@ -19,6 +23,45 @@ class DestiniPage extends StatefulWidget {
 class _DestiniPageState extends State<DestiniPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.red,
+          shape: BoxShape.rectangle,
+          image: DecorationImage(
+              image: AssetImage("images/background.png"), fit: BoxFit.cover)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+              flex: 5,
+              child: Center(
+                child: Text(
+                  "Story text will go here.",
+                  style: TextStyle(fontSize: 25.0, color: Colors.white),
+                ),
+              )),
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              onPressed: () {},
+              child: Text("Choice 1",
+                  style: TextStyle(fontSize: 20.0, color: Colors.white)),
+              color: Colors.red,
+            ),
+          )),
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              onPressed: () {},
+              child: Text("Choice 2",
+                  style: TextStyle(fontSize: 20.0, color: Colors.white)),
+              color: Colors.blue,
+            ),
+          ))
+        ],
+      ),
+    );
   }
 }
